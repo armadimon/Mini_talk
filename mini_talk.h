436764d59ce9b5fedf92276174d2f04aff445aee
ahn jun geol <juahn@ahnui-MacBookAir.local>
2021-10-05 17:15:38 +0900

    first

---
+++ mini_talk.h
@@ -0,0 +1,13 @@
+#ifndef MINI_TALK_H
+# define MINI_TALK_H
+
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+char		*ft_strdup(const char *src);
+size_t		ft_strlen(const char *str);
+
+#endif
